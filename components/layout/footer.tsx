0af2f097b39e5f6a2f45394ee293fd96246e602f
hanaheng <hannaulia766@gmail.com>
2025-07-03 16:14:59 +0700

    Update footer.tsx

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -84,7 +84,7 @@ export function Footer() {
         </div>
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2025 Secondhan
+          <p>&copy; 2025 Secondhand
           d. Semua hak dilindungi.</p>
         </div>
       </div>
